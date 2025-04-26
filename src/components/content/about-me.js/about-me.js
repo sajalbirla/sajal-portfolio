5b19d43e8731e38e4ee1e81baf9d1ef581155fff
SajalBirla <70742093+sajalbirla@users.noreply.github.com>
2025-04-26 00:09:04 +0000

    updating the work done from local

--- src/components/content/about-me.js/about-me.js
+++ src/components/content/about-me.js/about-me.js
@@ -1,27 +1,28 @@
 import React from "react";
-// import SocialMedia from "../../socials/s¸ocials";
+import SelfMemoji from '../../assets/memoji_self.jpeg';
+import {Urlconfig} from '../../../constant/config';
 
 const AboutMe = () => {
   const DownloadResume = () => {
-    window.location.href =
-      "/portfolio-website/Resume_Sajal_Birla_Developer.pdf";
+    window.location.href = Urlconfig.ResumeUrl;
   };
+  
   return (
-    <>
-      <div className="about">
-        <div className="about-details">
-          {/* <span className="about-details-tag">I'M</span> */}
-          <span className="about-details-name">Sajal Birla</span>
-          <div className="about-details-role">Software Developer - ReactJs</div>
-          <button className="about-details-resume">Contact Me</button>
+    <div className="about">
+      <div className="about-details">
+        <span className="about-details-name">Sajal Birla</span>
+        <div className="about-details-role">Software Development Engineer</div>
+        <div>
+          <button className="about-details-contact">Contact Me</button>
           <button className="about-details-resume" onClick={DownloadResume}>
             Resume
           </button>
-          {/* <SocialMedia /> */}
         </div>
-        <div className="about-details-image"></div>
       </div>
-    </>
+      <div className="about-details-image">
+        <img src={SelfMemoji} />
+      </div>
+    </div>
   );
 };
 export default AboutMe;
